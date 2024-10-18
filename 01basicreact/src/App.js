3a4c8ece93f24d8c24e1a738fbc94820baecb9e8
Vishal-sikchi-088 <vishalsikchi088@gmail.com>
2024-10-18 20:04:46 +0530

    basic of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai"
+function App() {
+  return ( 
+  <>
+   <Chai></Chai>
+   <hi>Vishal | Vishal</hi>
+  </>
+   
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+function Chai() {
+    return(  <>
+            <h1>chai is ready</h1>
+            <h2>chai is not ready</h2>
+            </>
+    )
+}
+
+export default Chai
